468039407837738d6273a52c56576148252992de
Jorrit Meulenbeld <jorrit.meulenbeld@outlook.com>
2017-06-07 20:12:07 +0200

    First commit

---
+++ ChatClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChatClient.ChatService;
+
+namespace ChatClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChatServiceClient chatProxy = new ChatServiceClient();
+            Console.WriteLine("Please enter your name:");
+            string name = Console.ReadLine();
+            while (true)
+            {
+                Console.WriteLine("Type a note (or hit enter to quit):");
+                string note = Console.ReadLine();
+                if (string.IsNullOrEmpty(note))
+                {
+                    break;
+                }
+                chatProxy.PostNote(name, note);
+            }
+        }
+    }
+}
---
+++ ChatHost/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ChatServerLibrary;
+
+namespace ChatHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(ChatService)))
+            {
+                host.Open();
+                Console.WriteLine("Service ready");
+                Console.ReadKey();
+            }
+        }
+    }
+}
